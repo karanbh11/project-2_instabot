c510e03e4540d4d90eaeb4c5c697d1a1af225459
karan bhardwaj <karan.bhardwaj1108@gmail.com>
2017-07-08 17:14:25 +0530

    Modified self_info to improve readability of data

--- instabot.py
+++ instabot.py
@@ -11,6 +11,17 @@ def self_info():
     req_url = BASE_URL + 'users/self/?access_token=' + ACCESS_TOKEN
     print('The request URL is %s' % req_url)
     user_info = requests.get(req_url).json()
-    print(user_info)
+
+    # To output the recieved data in more user-friendly way
+    if user_info['meta']['code'] == 200:
+        if (len(user_info['data']) > 0):
+            print('Username: %s' % (user_info['data']['username']))
+            print('No. of followers: %s' % (user_info['data']['counts']['followed_by']))
+            print('No. of people you are following: %s' % (user_info['data']['counts']['follows']))
+            print('No. of posts: %s' % (user_info['data']['counts']['media']))
+        else:
+            print('User does not exist!')
+    else:
+        print('Status code other than 200 received!')
 
 self_info()
